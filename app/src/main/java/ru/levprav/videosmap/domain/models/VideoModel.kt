fdfb4c7f0aa3195eb6ed5082ac8a0af3049df692
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-03 19:26:26 +0300

    add video model

---
+++ app/src/main/java/ru/levprav/videosmap/domain/models/VideoModel.kt
@@ -0,0 +1,19 @@
+package ru.levprav.videosmap.domain.models
+
+import android.location.Location
+import java.util.Date
+
+data class VideoModel(
+    val id: String, // Video's ID
+    val url: String, // URL of the video in full size
+    val imageUrl: String, // URL of the image of the first frame of the video in full size
+    val thumbnailUrl: String, // URL of the thumbnail of the first frame of the video
+    val createdAt: Date, // Timestamp of when the video was posted
+    val description: String, // Text description of the video. Used to perform keyword search as well
+    val userId: String, // ID of the user who have posted the video
+    val isFollowing: Boolean, // Whether the logged in user is following the creator of this video
+    val position: Location, // Coordinates of the position of the video
+    val likeCount: Int, // Video likes count
+    val commentCount: Int, // Video comments count
+    val haveLiked: Boolean, // Whether the logged in user liked video
+)
