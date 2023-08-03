c7b92cb1b0d8c8cd0594db827c80c54d42dd347b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-03 19:47:25 +0300

    add video repository

--- app/src/main/java/ru/levprav/videosmap/domain/repository/VideoRepository.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/repository/VideoRepository.kt
@@ -1,6 +1,29 @@
 package ru.levprav.videosmap.domain.repository
 
-interface VideoRepository {
+import android.location.Location
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import ru.levprav.videosmap.domain.models.VideoModel
+import ru.levprav.videosmap.domain.util.Resource
+import java.io.File
 
+interface VideoRepository {
+    suspend fun getVideosFromLocation(location: LatLng): Resource<List<VideoModel>>
+
+    suspend fun getVideosInBoundingBox(bounds: LatLngBounds): Resource<List<VideoModel>>
+
+    suspend fun getVideosFromUid(uid: String): Resource<List<VideoModel>>
+
+    suspend fun saveVideo(video: VideoModel): Resource<String>
+    suspend fun like(video: VideoModel): Resource<String>
+    suspend fun unlike(video: VideoModel): Resource<String>
+    suspend fun deleteVideo(videoId: String): Resource<String>
+
+    suspend fun searchVideo(videoId: String): Resource<List<VideoModel>>
+
+    suspend fun shareVideo(videoId: String): Resource<String>
+    suspend fun getVideoFile(): Resource<File>
+    suspend fun getVideoLocation(videoPath: String): Resource<LatLng>
+    suspend fun getNewVideos(): Resource<List<VideoModel>>
 
 }
