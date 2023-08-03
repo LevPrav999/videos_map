0114e86f5d1040abdc3da1d4c889c14e95f04ae6
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-03 19:47:09 +0300

    Location -> LatLng

--- app/src/main/java/ru/levprav/videosmap/domain/models/VideoModel.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/models/VideoModel.kt
@@ -1,6 +1,7 @@
 package ru.levprav.videosmap.domain.models
 
 import android.location.Location
+import com.google.android.gms.maps.model.LatLng
 import java.util.Date
 
 data class VideoModel(
@@ -12,7 +13,7 @@ data class VideoModel(
     val description: String, // Text description of the video. Used to perform keyword search as well
     val userId: String, // ID of the user who have posted the video
     val isFollowing: Boolean, // Whether the logged in user is following the creator of this video
-    val position: Location, // Coordinates of the position of the video
+    val position: LatLng, // Coordinates of the position of the video
     val likeCount: Int, // Video likes count
     val commentCount: Int, // Video comments count
     val haveLiked: Boolean, // Whether the logged in user liked video
