77be7dc63046a03e6fb3cae0e0f452bbb8aa3237
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-04 19:31:04 +0300

    update user repository

--- app/src/main/java/ru/levprav/videosmap/domain/repository/UserRepository.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/repository/UserRepository.kt
@@ -5,18 +5,18 @@ import ru.levprav.videosmap.domain.util.Resource
 
 interface UserRepository {
 
-    suspend fun signUp(email: String, password: String): Resource<String>
-    suspend fun signIn(email: String, password: String): Resource<String>
+    suspend fun signUp(email: String, password: String): Resource<Unit>
+    suspend fun signIn(email: String, password: String): Resource<Unit>
 
     suspend fun getMyProfile(): Resource<UserModel>
 
     suspend fun getProfileDetail(targetUid: String): Resource<UserModel>
 
-    suspend fun saveProfile(profile: UserModel): Resource<String>
+    suspend fun saveProfile(profile: UserModel): Resource<Unit>
 
-    suspend fun follow(followedUid: String): Resource<String>
+    suspend fun follow(followedUid: String): Resource<Unit>
 
-    suspend fun unfollow(followedUid: String): Resource<String>
+    suspend fun unfollow(followedUid: String): Resource<Unit>
 
     suspend fun getFollowers(uid: String): Resource<List<UserModel>>
     suspend fun getFollowings(uid: String): Resource<List<UserModel>>
