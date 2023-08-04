13a02faca25057ca37fb9deecc380ae67738840e
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-04 19:30:48 +0300

    add fromMap and toMap

--- app/src/main/java/ru/levprav/videosmap/domain/models/UserModel.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/models/UserModel.kt
@@ -6,7 +6,33 @@ data class UserModel(
     val description: String, // User Bio
     val imageUrl: String, // User Avatar Url
     val isFollowing: Boolean, // Is current user following this user
-    val followers: MutableSet<String>, // List of User's followers
-    val following: MutableSet<String>, // List of User's following
+    val followers: Set<Char>, // List of User's followers
+    val following: Set<Char>, // List of User's following
     val likeCount: Int, // Count of User videos' likes
 )
+
+fun UserModel.toMap(): Map<String, Any> {
+    return mapOf(
+        "id" to id,
+        "name" to name,
+        "description" to description,
+        "imageUrl" to imageUrl,
+        "isFollowing" to isFollowing,
+        "followers" to followers.toList(),
+        "following" to following.toList(),
+        "likeCount" to likeCount
+    )
+}
+
+fun Map<String, Any>.toUserModel(): UserModel {
+    return UserModel(
+        id = this["id"].toString(),
+        name = this["name"].toString(),
+        description = this["description"].toString(),
+        imageUrl = this["imageUrl"].toString(),
+        isFollowing = this["isFollowing"].toString().toBoolean(),
+        followers = this["followers"].toString().toSet(),
+        following = this["following"].toString().toSet(),
+        likeCount = this["likeCount"].toString().toInt()
+    )
+}
