08989ae246b1351cb19e3661da329b13bb268916
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-05 21:35:47 +0300

    add dependency injection

---
+++ app/src/main/java/ru/levprav/videosmap/di/LocationModule.kt
@@ -0,0 +1,23 @@
+package ru.levprav.videosmap.di
+
+
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import ru.levprav.videosmap.domain.location.LocationTracker
+import ru.levprav.videosmap.data.location.DefaultLocationTracker
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
+
