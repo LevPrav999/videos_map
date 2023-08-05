941a5ad8ff5bdb0fac44e4881b7aabf0f50c3f55
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-05 21:35:14 +0300

    update user api

--- app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
+++ app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
@@ -3,15 +3,19 @@ package ru.levprav.videosmap.data.remote
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.tasks.await
 import ru.levprav.videosmap.domain.models.UserModel
+import javax.inject.Inject
 
-class UserApi {
+class UserApi @Inject constructor(){
 
     var firebaseFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
     var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     suspend fun signUp(email: String, password: String){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+    }
+
+    suspend fun signIn(email: String, password: String){
         firebaseAuth.signInWithEmailAndPassword(email, password)
     }
 }
