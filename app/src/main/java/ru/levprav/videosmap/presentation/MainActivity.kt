4ab70183695c0da6592feda238997eb5036a129b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-05 21:36:11 +0300

    update main activity [test]

--- app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
@@ -3,18 +3,23 @@ package ru.levprav.videosmap.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import dagger.hilt.android.AndroidEntryPoint
 import ru.levprav.videosmap.presentation.ui.theme.VideosMapTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SignInViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,7 +29,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(viewModel)
+
                 }
             }
         }
@@ -32,19 +38,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        fontSize = 38.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VideosMapTheme {
-        Greeting("Android")
+fun Greeting(vm: SignInViewModel) {
+    Button(onClick = {
+        vm.signUp("testttt@gmail.com", "12345")
+    }) {
+        Text(text = "Sign Up")
     }
 }
