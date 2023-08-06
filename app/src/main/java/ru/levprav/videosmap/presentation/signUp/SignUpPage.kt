702536c5fcd74d77ff9351fdba6a825f08975c3c
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-06 19:27:36 +0300

    add signUp page [test]

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/signUp/SignUpPage.kt
@@ -0,0 +1,18 @@
+package ru.levprav.videosmap.presentation.signUp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun SignUpScreen(navController: NavController) {
+    Column {
+        Text(text = "Welcome!")
+        Button(onClick = { navController.navigate("secondScreen") }) {
+            Text(text = "Continue")
+        }
+    }
+}
