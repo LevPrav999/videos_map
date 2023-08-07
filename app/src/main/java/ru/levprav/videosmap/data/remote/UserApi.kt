96b8eb9be770fe4ed6415c11613da6da4bf0b1ce
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-07 20:42:48 +0300

    add dispatcher and new methods

--- app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
+++ app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
@@ -1,8 +1,13 @@
 package ru.levprav.videosmap.data.remote
 
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import ru.levprav.videosmap.domain.models.UserModel
 import javax.inject.Inject
 
@@ -11,9 +16,16 @@ class UserApi @Inject constructor(){
     var firebaseFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
     var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
-    suspend fun signUp(email: String, password: String){
+    suspend fun signUp(email: String, password: String) = withContext(Dispatchers.IO) {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
     }
+
+    suspend fun checkUserExists(email: String): Boolean = withContext(Dispatchers.IO) {
+        val task = firebaseAuth.fetchSignInMethodsForEmail(email)
+        Tasks.await(task)
+        task.result?.signInMethods?.size == 0
+    }
+
 
     suspend fun signIn(email: String, password: String){
         firebaseAuth.signInWithEmailAndPassword(email, password)
