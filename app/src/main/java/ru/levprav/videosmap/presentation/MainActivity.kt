db05d9d294c3497c64cd9ae5c075eb5763c1f813
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-07 20:44:09 +0300

    add navigation

--- app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
@@ -12,36 +12,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import ru.levprav.videosmap.presentation.signUp.SignUpPage
 import ru.levprav.videosmap.presentation.ui.theme.VideosMapTheme
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: SignInViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VideosMapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting(viewModel)
-
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "signUp"){
+                composable("signUp") {
+                    SignUpPage(navController = navController, hiltViewModel())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(vm: SignInViewModel) {
-    Button(onClick = {
-        vm.signUp("testttt@gmail.com", "123456")
-    }) {
-        Text(text = "Sign Up")
     }
 }
