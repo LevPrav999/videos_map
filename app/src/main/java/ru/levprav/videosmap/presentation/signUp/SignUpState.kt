eff5b0802f2d0d047ede57f8c2fd4c2b76fd8945
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-07 20:43:43 +0300

    add signUp state

--- app/src/main/java/ru/levprav/videosmap/presentation/signUp/SignUpState.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/signUp/SignUpState.kt
@@ -1,6 +1,7 @@
 package ru.levprav.videosmap.presentation.signUp
 
 data class SignUpState(
+    val data: List<String> = listOf("", ""),
     val isLoading: Boolean = false,
     val error: String? = null
 )
