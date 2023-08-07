88fc95e67671dbc56b9a10ca019c7a4e5b2c6247
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-07 20:43:28 +0300

    add signUp page

--- app/src/main/java/ru/levprav/videosmap/presentation/signUp/SignUpPage.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/signUp/SignUpPage.kt
@@ -1,17 +1,98 @@
 package ru.levprav.videosmap.presentation.signUp
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 
+@SuppressLint("CoroutineCreationDuringComposition")
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun SignUpScreen(navController: NavController) {
-    Column {
-        Text(text = "Welcome!")
-        Button(onClick = { navController.navigate("secondScreen") }) {
-            Text(text = "Continue")
+fun SignUpPage(navController: NavController, viewModel: SignUpViewModel) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    LaunchedEffect(viewModel.state.error) {
+        viewModel.state.error?.let { error ->
+            snackbarHostState.showSnackbar(error)
         }
     }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Email
+                ),
+                textStyle = MaterialTheme.typography.bodyMedium,
+                placeholder = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
+                textStyle = MaterialTheme.typography.bodyMedium,
+                placeholder = { Text("Password") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text("Back")
+                }
+
+                Button(
+                    onClick = {
+                        viewModel.signUp(email, password)
+                        keyboardController?.hide()
+                    },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text("Sign Up")
+                }
+            }
+        }
+    }
+    SnackbarHost(snackbarHostState)
 }
