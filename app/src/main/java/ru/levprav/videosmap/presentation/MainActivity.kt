6a3528c0697461a7c6b3ca15155ac6cc6c04928b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-10 17:28:36 +0300

    rename path

--- app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
@@ -3,22 +3,12 @@ package ru.levprav.videosmap.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import ru.levprav.videosmap.presentation.signUp.AuthPage
-import ru.levprav.videosmap.presentation.ui.theme.VideosMapTheme
+import ru.levprav.videosmap.presentation.auth.AuthPage
 
 
 @AndroidEntryPoint
