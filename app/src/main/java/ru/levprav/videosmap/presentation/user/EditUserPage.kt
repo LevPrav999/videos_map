dfe82c1044a4481995505ba9988a5feec58c6cbb
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-10 18:08:15 +0300

    add edit user page

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/user/EditUserPage.kt
@@ -0,0 +1,9 @@
+package ru.levprav.videosmap.presentation.user
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun EditUserPage(navController: NavController, viewModel: EditUserViewModel) {
+
+}
