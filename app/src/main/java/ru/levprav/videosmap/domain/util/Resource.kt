a60127c65b3f72581a108240bfd0c433f4c5d22f
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-10 17:27:35 +0300

    add loading

--- app/src/main/java/ru/levprav/videosmap/domain/util/Resource.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/util/Resource.kt
@@ -1,6 +1,8 @@
 package ru.levprav.videosmap.domain.util
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+sealed class Resource<T>(val data: T? = null, val message: String? = null, val isLoading: Boolean? = false) {
     class Success<T>(data: T?): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+
+    class Loading<T> : Resource<T>()
 }
