7a1ac315ba5df9a0505441cc10896ef3fc621073
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-10 17:27:02 +0300

    liveData to flow in repository

--- app/src/main/java/ru/levprav/videosmap/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/ru/levprav/videosmap/data/repository/UserRepositoryImpl.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import ru.levprav.videosmap.data.remote.UserApi
 import ru.levprav.videosmap.domain.models.UserModel
 import ru.levprav.videosmap.domain.repository.UserRepository
 import ru.levprav.videosmap.domain.util.Resource
 import javax.inject.Inject
+import kotlin.random.Random
 
 class UserRepositoryImpl @Inject constructor(
     private val api: UserApi
@@ -16,65 +21,31 @@ class UserRepositoryImpl @Inject constructor(
 
     private val signUpLiveData = MutableLiveData<Resource<Unit>>()
     private val signInLiveData = MutableLiveData<Resource<Unit>>()
-    override suspend fun signUp(email: String, password: String, passwordConfirm: String): LiveData<Resource<Unit>> {
-        try {
-            if (email.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()) {
-                signUpLiveData.postValue(Resource.Error("Please fill in all fields"))
-            }
-
-            else if (password != passwordConfirm){
-                signUpLiveData.postValue(Resource.Error("Passwords don't match"))
-            }
-
-            else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                signUpLiveData.postValue(Resource.Error("Incorrect Email format"))
-            }else{
-                val isUserExists = api.checkUserExists(email)
-                if (isUserExists){
-                    signUpLiveData.postValue(Resource.Error("Email is being used by another account"))
-                }else{
-                    api.signUp(email, password)
-                    signUpLiveData.postValue(Resource.Success(Unit))
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            signUpLiveData.postValue(Resource.Error(e.message ?: "Unknown error"))
-        }
-
-        return signUpLiveData
+    override suspend fun signUp(email: String, password: String, passwordConfirm: String): Flow<Resource<Unit>> {
+        TODO()
     }
 
-    override suspend fun signIn(email: String, password: String): LiveData<Resource<Unit>> {
-        try {
-            if (email.isEmpty() || password.isEmpty()) {
-                signInLiveData.postValue(Resource.Error("Please fill in all fields"))
-            }
+    override suspend fun signIn(email: String, password: String): Flow<Resource<Unit>> = flow {
+        emit(Resource.Loading())
 
-            else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                signInLiveData.postValue(Resource.Error("Incorrect Email format"))
-            }else{
-                val isUserExists = api.checkUserExists(email)
-                if (!isUserExists){
-                    signInLiveData.postValue(Resource.Error("User with this Email doesn't exists"))
-                }else{
-                    api.signIn(email, password).addOnCompleteListener{
-                        task ->
-                        if(task.isSuccessful){
-                            signInLiveData.postValue(Resource.Success(Unit))
-                        }else{
-                            signInLiveData.postValue(Resource.Error(task.exception?.message.toString()))
-                        }
-                    }
+        if (email.isEmpty() || password.isEmpty()) {
+            emit(Resource.Error("Please fill in all fields"))
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emit(Resource.Error("Incorrect Email format"))
+        } else {
+            val isUserExists = api.checkUserExists(email)
+            if (!isUserExists) {
+                emit(Resource.Error("User with this Email doesn't exists"))
+            } else {
+                try {
+                    api.signIn(email, password)
+                    emit(Resource.Success(Unit))
+                } catch (e: Exception) {
+                    emit(Resource.Error(e.message ?: "Unknown error"))
                 }
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            signInLiveData.postValue(Resource.Error(e.message ?: "Unknown error"))
         }
-
-        return signInLiveData
-    }
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun getMyProfile(): Resource<UserModel> {
         TODO("Not yet implemented")
