08d30fcaea3091fc0ab7e22ed1084162269ff0cb
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-11 22:10:55 +0300

    add new methods to user repository

--- app/src/main/java/ru/levprav/videosmap/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/ru/levprav/videosmap/data/repository/UserRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package ru.levprav.videosmap.data.repository
 
+import com.google.android.gms.tasks.Tasks
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import ru.levprav.videosmap.data.remote.UserApi
 import ru.levprav.videosmap.domain.models.UserModel
+import ru.levprav.videosmap.domain.models.toUserModel
 import ru.levprav.videosmap.domain.repository.UserRepository
 import ru.levprav.videosmap.domain.util.Resource
 import javax.inject.Inject
@@ -28,7 +30,7 @@ class UserRepositoryImpl @Inject constructor(
         } else if (password != passwordConfirm) {
             emit(Resource.Error("Passwords don't match"))
         } else {
-            val isUserExists = api.checkUserExists(email)
+            val isUserExists = api.checkUserAuth(email)
             if (isUserExists) {
                 emit(Resource.Error("User with this Email already exists"))
             } else {
@@ -50,7 +52,7 @@ class UserRepositoryImpl @Inject constructor(
         } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             emit(Resource.Error("Incorrect Email format"))
         } else {
-            val isUserExists = api.checkUserExists(email)
+            val isUserExists = api.checkUserAuth(email)
             if (!isUserExists) {
                 emit(Resource.Error("User with this Email doesn't exists"))
             } else {
@@ -64,17 +66,55 @@ class UserRepositoryImpl @Inject constructor(
         }
     }.flowOn(Dispatchers.IO)
 
-    override suspend fun getMyProfile(): Resource<UserModel> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getMyProfile(): Flow<Resource<UserModel>> = flow<Resource<UserModel>> {
+        emit(Resource.Loading())
+        api.firebaseAuth.currentUser?.let {
+            val result = api.getUserDocumentById(it.uid)
+            Tasks.await(result)
+            emit(Resource.Success(result.result.data?.toUserModel()))
+        } ?: emit(Resource.Error("User not found"))
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun getProfileDetail(targetUid: String): Resource<UserModel> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun saveProfile(profile: UserModel): Resource<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun saveProfile(name: String?, description: String?, imageUrl: String?, isFollowing: Boolean?, followers: Set<Char>?, following: Set<Char>?, likeCount: Int?): Flow<Resource<Unit>> = flow {
+        emit(Resource.Loading())
+        val id = api.firebaseAuth.currentUser!!.uid
+        if(api.checkUserDocumentExists(id)){
+
+            val task = api.getUserDocumentById(id)
+            Tasks.await(task)
+            val oldUser = task.result.data!!.toUserModel()
+
+            val user = UserModel(
+                id = id,
+                name = name ?: oldUser.name,
+                description = description ?: oldUser.description,
+                imageUrl = imageUrl ?: oldUser.imageUrl,
+                isFollowing = isFollowing ?: oldUser.isFollowing,
+                followers = followers ?: oldUser.followers,
+                following = following ?: oldUser.following,
+                likeCount = likeCount ?: oldUser.likeCount,
+            )
+            api.updateUserDocument(user)
+
+        }else{
+            val user = UserModel(
+                id = id,
+                name = name!!,
+                description = description!!,
+                imageUrl = imageUrl!!,
+                isFollowing = false,
+                followers = setOf(),
+                following = setOf(),
+                likeCount = 0,
+            )
+            api.addUserDocument(user)
+        }
+        emit(Resource.Success(Unit))
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun follow(followedUid: String): Resource<Unit> {
         TODO("Not yet implemented")
