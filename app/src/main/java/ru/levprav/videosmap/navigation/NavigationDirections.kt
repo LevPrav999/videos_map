371b00d9bd3aab2c01f1eaac88f937e4f056f34d
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-14 17:09:27 +0300

    add navigation directions

---
+++ app/src/main/java/ru/levprav/videosmap/navigation/NavigationDirections.kt
@@ -0,0 +1,44 @@
+package ru.levprav.videosmap.navigation
+
+import androidx.navigation.NamedNavArgument
+
+object NavigationDirections {
+
+    val default = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = ""
+
+        override val clearStack = true
+
+    }
+
+    val authentication  = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "auth"
+
+        override val clearStack = false
+
+    }
+
+    val editUser = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "edit_user"
+
+        override val clearStack = true
+    }
+
+    val mainScreen = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "tabs_page"
+
+        override val clearStack = true
+    }
+}
