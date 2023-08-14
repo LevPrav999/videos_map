c99bd1db9b9950b189659fcc804fad83f24aed4d
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-14 17:10:46 +0300

    update navigation in main activity

--- app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
@@ -5,33 +5,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import ru.levprav.videosmap.navigation.NavigationDirections
+import ru.levprav.videosmap.navigation.NavigationManager
 import ru.levprav.videosmap.presentation.auth.AuthPage
 import ru.levprav.videosmap.presentation.main.TabsPage
 import ru.levprav.videosmap.presentation.edituser.EditUserPage
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var navigationManager: NavigationManager
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "auth") {
-                composable("auth") {
-                    AuthPage(navController = navController, hiltViewModel())
+            LaunchedEffect(navigationManager.commands){
+                navigationManager.commands.collect{ command ->
+                    if (command.destination.isNotEmpty()) {
+                        if(command.clearStack){
+                            navController.popBackStack()
+                        }
+                        navController.navigate(command.destination)
+                    }
+                }
+            }
+            NavHost(navController = navController, startDestination = NavigationDirections.authentication.destination) {
+                composable(NavigationDirections.authentication.destination) {
+                    AuthPage(hiltViewModel())
                 }
 
-                composable("edit_user") {
-                    EditUserPage(navController = navController, hiltViewModel())
+                composable(NavigationDirections.editUser.destination) {
+                    EditUserPage(hiltViewModel())
                 }
-                composable("tabs_page") {
+                composable(NavigationDirections.mainScreen.destination) {
                     TabsPage()
                 }
             }
