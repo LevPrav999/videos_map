a92a143543f9364a155d3242ab377e31a48a660b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-15 22:04:26 +0300

    mutableStateOf -> mutableIntStateOf

--- app/src/main/java/ru/levprav/videosmap/presentation/auth/AuthPage.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/auth/AuthPage.kt
@@ -2,22 +2,18 @@ package ru.levprav.videosmap.presentation.auth
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
+import ru.levprav.videosmap.presentation.auth.components.AuthDialog
+import ru.levprav.videosmap.presentation.auth.components.ChoiceDialog
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun AuthPage(viewModel: AuthViewModel) {
-    var dialogIndex by remember { mutableStateOf(0) } // 0 - choice 1 - signUp 2 - signIn
+    var dialogIndex by remember { mutableIntStateOf(0) } // 0 - choice 1 - signUp 2 - signIn
 
 
     val keyboardController = LocalSoftwareKeyboardController.current
@@ -80,137 +76,4 @@ fun AuthPage(viewModel: AuthViewModel) {
 
 
     SnackbarHost(snackbarHostState)
-}
-
-@Composable
-fun AuthDialog(
-    isLoading: Boolean,
-    isSignUp: Boolean,
-    email: String,
-    password: String,
-    passwordConfirm: String,
-    onEmailChanged: (String) -> Unit,
-    onPasswordChanged: (String) -> Unit,
-    onPasswordConfirmChanged: (String) -> Unit,
-    onBackPressed: () -> Unit,
-    onButtonPressed: () -> Unit
-) {
-
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            TextField(
-                value = email,
-                onValueChange = {
-                    onEmailChanged(it)
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Email
-                ),
-                textStyle = MaterialTheme.typography.bodyMedium,
-                placeholder = { Text("Email") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            )
-
-            TextField(
-                value = password,
-                onValueChange = { onPasswordChanged(it) },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Password
-                ),
-                textStyle = MaterialTheme.typography.bodyMedium,
-                placeholder = { Text("Password") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            )
-            if (isSignUp) {
-                TextField(
-                    value = passwordConfirm,
-                    onValueChange = { onPasswordConfirmChanged(it) },
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Password
-                    ),
-                    textStyle = MaterialTheme.typography.bodyMedium,
-                    placeholder = { Text("Confirm password") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                )
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Button(
-                    onClick = onBackPressed,
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    Text("Back")
-                }
-
-                Button(
-                    onClick = onButtonPressed,
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    if (isLoading) {
-                        CircularProgressIndicator(color = Color.White)
-                    } else {
-                        Text(if (isSignUp) "Sign Up" else "Sign In")
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ChoiceDialog(onSignInClick: () -> Unit, onSignUpClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to VideosMap",
-            style = MaterialTheme.typography.headlineLarge,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Button(
-            onClick = onSignInClick,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
-        ) {
-            Text(text = "Sign In", color = Color.White)
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = onSignUpClick,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
-        ) {
-            Text(text = "Sign Up", color = Color.White)
-        }
-    }
 }
