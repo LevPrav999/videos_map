a2eabdc6fdd8d3a56fd219d487d90787c0b534a5
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-15 21:39:30 +0300

    add coroutines to user api

--- app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
+++ app/src/main/java/ru/levprav/videosmap/data/remote/UserApi.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.levprav.videosmap.domain.models.UserModel
 import ru.levprav.videosmap.domain.models.toMap
+import ru.levprav.videosmap.domain.models.toUserModel
 import java.io.File
 import javax.inject.Inject
 import kotlin.coroutines.resume
@@ -29,9 +30,17 @@ class UserApi @Inject constructor() {
     }
 
     suspend fun signIn(email: String, password: String): FirebaseUser? = withContext(Dispatchers.IO) {
-        val task = firebaseAuth.signInWithEmailAndPassword(email, password)
-        Tasks.await(task)
-        task.result.user
+        val signInTask = firebaseAuth.signInWithEmailAndPassword(email, password)
+
+        suspendCoroutine { continuation ->
+            signInTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    continuation.resume(task.result.user)
+                } else {
+                    continuation.resumeWithException(task.exception ?: Exception("Error Sign In"))
+                }
+            }
+        }
     }
 
 
@@ -43,8 +52,24 @@ class UserApi @Inject constructor() {
         firebaseFirestore.collection("users").document(user.id).update(user.toMap())
     }
 
-    suspend fun getUserDocumentById(uid: String) = withContext(Dispatchers.IO){
-        firebaseFirestore.collection("users").document(uid).get()
+    suspend fun getUserDocumentById(uid: String): UserModel = withContext(Dispatchers.IO){
+
+
+        suspendCoroutine { continuation ->
+            firebaseFirestore.collection("users").document(uid).get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val snapshot = task.result
+                    val user = snapshot?.data?.toUserModel()
+                    if(user == null){
+                        continuation.resumeWithException(task.exception ?: Exception("User not found"))
+                    }else{
+                        continuation.resume(user)
+                    }
+                } else {
+                    continuation.resumeWithException(task.exception ?: Exception("Error getting user"))
+                }
+            }
+        }
     }
 
     suspend fun saveUserAvatar(storagePath: String, image: Uri): String = withContext(Dispatchers.IO){
@@ -73,15 +98,31 @@ class UserApi @Inject constructor() {
     }
 
     suspend fun checkUserAuth(email: String): Boolean = withContext(Dispatchers.IO) {
-        val task = firebaseAuth.fetchSignInMethodsForEmail(email)
-        Tasks.await(task)
-        task.result?.signInMethods?.size != 0
+        val fetchTask = firebaseAuth.fetchSignInMethodsForEmail(email)
+
+        suspendCoroutine { continuation ->
+            fetchTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    continuation.resume(task.result?.signInMethods?.size != 0)
+                } else {
+                    continuation.resumeWithException(task.exception ?: Exception("Error checking auth"))
+                }
+            }
+        }
     }
 
     suspend fun checkUserDocumentExists(uid: String): Boolean = withContext(Dispatchers.IO) {
-        val task = firebaseFirestore.collection("users").document(uid).get()
-        Tasks.await(task)
-        task.result?.data != null
+        val fetchTask = firebaseFirestore.collection("users").document(uid).get()
+
+        suspendCoroutine { continuation ->
+            fetchTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    continuation.resume(task.result?.data != null)
+                } else {
+                    continuation.resumeWithException(task.exception ?: Exception("Error checking user documents"))
+                }
+            }
+        }
     }
 
 }
