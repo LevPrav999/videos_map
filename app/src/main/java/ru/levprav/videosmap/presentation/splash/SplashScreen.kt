894affb9988fd36186f7529834f1b5c4f7ba37f7
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-15 21:55:02 +0300

    add splash screen

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/splash/SplashScreen.kt
@@ -0,0 +1,17 @@
+package ru.levprav.videosmap.presentation.splash
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun SplashScreen(viewModel: SplashViewModel) {
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Splash")
+    }
+}
