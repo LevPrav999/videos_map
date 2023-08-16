c60b55c32b0b7faa3eafa3e53059072ba9a05ff2
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-16 22:40:32 +0300

    add simple profile page view

--- app/src/main/java/ru/levprav/videosmap/presentation/profile/ProfilePage.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/profile/ProfilePage.kt
@@ -1,8 +1,28 @@
 package ru.levprav.videosmap.presentation.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.AsyncImage
 
 @Composable
-fun ProfilePage() {
-
+fun ProfilePage(viewModel: ProfilePageViewModel) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        if(viewModel.state.isLoading){
+            CircularProgressIndicator()
+        }else if(viewModel.state.error != null){
+            Text(viewModel.state.error!!)
+        }else{
+            AsyncImage(model = viewModel.state.data.imageUrl, contentDescription = null)
+            viewModel.state.data.username?.let { Text(it) }
+            viewModel.state.data.description?.let { Text(it) }
+            Text(viewModel.state.data.followersCount.toString())
+            Text(viewModel.state.data.followingCount.toString())
+            Text(viewModel.state.data.likesCount.toString())
+        }
+    }
 }
