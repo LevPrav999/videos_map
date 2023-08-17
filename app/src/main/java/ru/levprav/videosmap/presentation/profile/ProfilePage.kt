d9a528e38da669b5b6426adaf7605fa9e2716897
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-17 22:02:18 +0300

    update profile view

--- app/src/main/java/ru/levprav/videosmap/presentation/profile/ProfilePage.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/profile/ProfilePage.kt
@@ -1,28 +1,71 @@
 package ru.levprav.videosmap.presentation.profile
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
 fun ProfilePage(viewModel: ProfilePageViewModel) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        if(viewModel.state.isLoading){
-            CircularProgressIndicator()
-        }else if(viewModel.state.error != null){
-            Text(viewModel.state.error!!)
-        }else{
-            AsyncImage(model = viewModel.state.data.imageUrl, contentDescription = null)
-            viewModel.state.data.username?.let { Text(it) }
-            viewModel.state.data.description?.let { Text(it) }
-            Text(viewModel.state.data.followersCount.toString())
-            Text(viewModel.state.data.followingCount.toString())
-            Text(viewModel.state.data.likesCount.toString())
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            // AsyncImage
+            AsyncImage(model = viewModel.state.data.imageUrl, contentDescription = null, modifier = Modifier
+                .size(100.dp)
+                .aspectRatio(1f)
+                .clip(CircleShape)
+                .border(2.dp, Color.Gray)
+            )
         }
+
+        item {
+            // Followers, Following, Likes
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Absolute.SpaceEvenly
+            ) {
+                StatisticItem(label = "Followers", count = viewModel.state.data.followersCount)
+                StatisticItem(label = "Following", count = viewModel.state.data.followingCount)
+                StatisticItem(label = "Likes", count = viewModel.state.data.likesCount)
+            }
+        }
+
+        item {
+            // Username and Description
+            viewModel.state.data.username?.let { username ->
+                Text(text = username, style = MaterialTheme.typography.headlineLarge)
+            }
+            viewModel.state.data.description?.let { description ->
+                Text(text = description, style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    }
+}
+
+@Composable
+fun StatisticItem(label: String, count: Int) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = count.toString(), style = MaterialTheme.typography.headlineSmall)
+        Text(text = label, style = MaterialTheme.typography.bodyMedium)
     }
 }
