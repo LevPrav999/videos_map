fcf6dbc29c0109b69b0b08f9e7d9913e433eba9f
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-19 15:28:52 +0300

    move navigation directions to the object

---
+++ app/src/main/java/ru/levprav/videosmap/navigation/TabsDirections.kt
@@ -0,0 +1,55 @@
+package ru.levprav.videosmap.navigation
+
+import androidx.navigation.NamedNavArgument
+
+object TabsDirections {
+    val map = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "map"
+
+        override val clearStack = false
+
+    }
+
+    val search = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "search"
+
+        override val clearStack = false
+
+    }
+
+    val add = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "add"
+
+        override val clearStack = false
+
+    }
+
+    val notifications = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "notifications"
+
+        override val clearStack = false
+
+    }
+
+    val profile = object : NavigationCommand {
+
+        override val arguments = emptyList<NamedNavArgument>()
+
+        override val destination = "profile"
+
+        override val clearStack = false
+
+    }
+}
