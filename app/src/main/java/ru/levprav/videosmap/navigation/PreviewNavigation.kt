c7ce5ef0ee742a7ae0097abd3a80cbbc2115a4e0
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-19 20:18:22 +0300

    add preview screen navigation

---
+++ app/src/main/java/ru/levprav/videosmap/navigation/PreviewNavigation.kt
@@ -0,0 +1,25 @@
+package ru.levprav.videosmap.navigation
+
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+object PreviewNavigation {
+
+    val KEY_URI = "uri"
+    val route = "preview/{$KEY_URI}"
+    val arguments = listOf(
+        navArgument(KEY_URI) { type = NavType.StringType }
+    )
+
+    fun previewScreen(
+        uri: String? = null
+    ) = object : NavigationCommand {
+
+
+        override val destination = "preview/{$uri}"
+        override val arguments = listOf(
+            navArgument(KEY_URI) { type = NavType.StringType }
+        )
+        override val clearStack = false
+    }
+}
