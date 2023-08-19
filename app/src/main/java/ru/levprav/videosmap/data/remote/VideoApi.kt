a357fd620ccf852ddcd7f44bb6cc516950ad10f2
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-19 20:16:05 +0300

    add save file and save bytes function to api

---
+++ app/src/main/java/ru/levprav/videosmap/data/remote/VideoApi.kt
@@ -0,0 +1,87 @@
+package ru.levprav.videosmap.data.remote
+
+import android.graphics.Bitmap
+import android.net.Uri
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.levprav.videosmap.domain.models.VideoModel
+import ru.levprav.videosmap.domain.models.toMap
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class VideoApi @Inject constructor() {
+
+    private var _firebaseFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var _firebaseStorage: FirebaseStorage = FirebaseStorage.getInstance()
+
+    suspend fun saveVideoToFirestore(video: VideoModel) = withContext(Dispatchers.IO) {
+        _firebaseFirestore.collection("videos").document(video.id).set(video.toMap())
+    }
+
+    suspend fun saveFileToStorage(storagePath: String, fileUri: Uri): String =
+        withContext(Dispatchers.IO) {
+            val storageReference = _firebaseStorage.reference
+
+            val imageRef = storageReference.child(storagePath)
+            val uploadTask = imageRef.putFile(fileUri)
+
+            suspendCoroutine { continuation ->
+                uploadTask.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val snapshot = task.result
+                        snapshot?.storage?.downloadUrl?.addOnCompleteListener { downloadUrlTask ->
+                            if (downloadUrlTask.isSuccessful) {
+                                val downloadUrl = downloadUrlTask.result.toString()
+                                continuation.resume(downloadUrl)
+                            } else {
+                                continuation.resumeWithException(
+                                    downloadUrlTask.exception
+                                        ?: Exception("Error getting download URL")
+                                )
+                            }
+                        }
+                    } else {
+                        continuation.resumeWithException(
+                            task.exception ?: Exception("Error uploading file")
+                        )
+                    }
+                }
+            }
+        }
+
+    suspend fun saveBytesToStorage(storagePath: String, bytes: ByteArray): String =
+        withContext(Dispatchers.IO) {
+            val storageReference = _firebaseStorage.reference
+
+            val imageRef = storageReference.child(storagePath)
+            val uploadTask = imageRef.putBytes(bytes)
+
+            suspendCoroutine { continuation ->
+                uploadTask.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val snapshot = task.result
+                        snapshot?.storage?.downloadUrl?.addOnCompleteListener { downloadUrlTask ->
+                            if (downloadUrlTask.isSuccessful) {
+                                val downloadUrl = downloadUrlTask.result.toString()
+                                continuation.resume(downloadUrl)
+                            } else {
+                                continuation.resumeWithException(
+                                    downloadUrlTask.exception
+                                        ?: Exception("Error getting download URL")
+                                )
+                            }
+                        }
+                    } else {
+                        continuation.resumeWithException(
+                            task.exception ?: Exception("Error uploading file")
+                        )
+                    }
+                }
+            }
+        }
+}
