e230d1e56323ddfff317a444430318e63aa10c81
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-19 15:29:09 +0300

    replace strings with object

--- app/src/main/java/ru/levprav/videosmap/presentation/main/components/Navigation.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/main/components/Navigation.kt
@@ -5,25 +5,26 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import ru.levprav.videosmap.navigation.TabsDirections
 import ru.levprav.videosmap.presentation.main.EmptyScreen
 import ru.levprav.videosmap.presentation.profile.ProfilePage
 
 @Composable
 fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "profile") {
-        composable("map") {
+    NavHost(navController = navController, startDestination = TabsDirections.profile.destination) {
+        composable(TabsDirections.map.destination) {
             EmptyScreen()
         }
-        composable("search") {
+        composable(TabsDirections.search.destination) {
             EmptyScreen()
         }
-        composable("add") {
+        composable(TabsDirections.add.destination) {
             EmptyScreen()
         }
-        composable("notifications") {
+        composable(TabsDirections.notifications.destination) {
             EmptyScreen()
         }
-        composable("profile") {
+        composable(TabsDirections.profile.destination) {
             ProfilePage(hiltViewModel())
         }
     }
