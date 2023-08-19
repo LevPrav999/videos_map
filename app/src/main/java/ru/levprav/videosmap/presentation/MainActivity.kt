0b78e2798842da93d163035401a4b5eb7745b081
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-19 20:20:43 +0300

    add video preview screen to nav graph

--- app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import ru.levprav.videosmap.navigation.NavigationDirections
 import ru.levprav.videosmap.navigation.NavigationManager
+import ru.levprav.videosmap.navigation.PreviewNavigation
 import ru.levprav.videosmap.presentation.auth.AuthPage
+import ru.levprav.videosmap.presentation.camera.VideoPreviewScreen
 import ru.levprav.videosmap.presentation.edituser.EditUserPage
 import ru.levprav.videosmap.presentation.main.TabsPage
 import ru.levprav.videosmap.presentation.splash.SplashScreen
@@ -57,6 +61,12 @@ class MainActivity : ComponentActivity() {
                 }
                 composable(NavigationDirections.splash.destination) {
                     SplashScreen(hiltViewModel())
+                }
+                composable(PreviewNavigation.route, arguments = PreviewNavigation.arguments)
+                { backStackEntry ->
+
+                    backStackEntry.arguments?.getString(PreviewNavigation.KEY_URI)
+                        ?.let { VideoPreviewScreen(it) }
                 }
             }
         }
