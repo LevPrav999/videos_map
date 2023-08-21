3e70ab3e65fd9e4a939572706fc1c443b18324e6
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-21 20:12:44 +0300

    add preview screen view model

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoPreviewViewModel.kt
@@ -0,0 +1,23 @@
+package ru.levprav.videosmap.presentation.camera
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.levprav.videosmap.navigation.DetailsNavigation
+import ru.levprav.videosmap.navigation.NavigationManager
+import ru.levprav.videosmap.navigation.PreviewNavigation
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoPreviewViewModel @Inject constructor(
+    private val navigationManager: NavigationManager
+) : ViewModel() {
+    fun navigate(uri: String) {
+        val uriEncoded = URLEncoder.encode(
+            uri,
+            StandardCharsets.UTF_8.toString()
+        )
+        navigationManager.navigate(DetailsNavigation.detailsScreen(uriEncoded))
+    }
+}
