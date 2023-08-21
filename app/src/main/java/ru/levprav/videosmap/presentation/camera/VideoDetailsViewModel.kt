987da248bd1377f3ab3f5f32c6910f73eca9bb5a
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-21 21:33:16 +0300

    add details view model

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoDetailsViewModel.kt
@@ -0,0 +1,46 @@
+package ru.levprav.videosmap.presentation.camera
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import ru.levprav.videosmap.data.repository.VideoRepositoryImpl
+import ru.levprav.videosmap.domain.util.Resource
+import ru.levprav.videosmap.navigation.NavigationManager
+import ru.levprav.videosmap.presentation.profile.ProfilePageState
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoDetailsViewModel @Inject constructor(
+    private val navigationManager: NavigationManager,
+    private val repository: VideoRepositoryImpl
+) : ViewModel() {
+
+    var state by mutableStateOf(VideoDetailsState())
+        private set
+
+    fun saveVideo(uri: String, bytes: ByteArray) {
+        viewModelScope.launch {
+            repository.saveVideo(uri, bytes).collect{
+                result ->
+                state = when(result){
+                    is Resource.Loading -> {
+                        state.copy(isLoading = true, error = null)
+                    }
+
+                    is Resource.Error -> {
+                        state.copy(isLoading = false, error = result.message)
+                    }
+
+                    else -> {
+                        state.copy(isLoading = false, error = null)
+                    }
+                }
+            }
+        }
+    }
+}
