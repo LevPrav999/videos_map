54051b2dd6d107328b2bce6b2bdaba5d7dd8b3eb
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-21 20:11:42 +0300

    add uriEncoded to view model

--- app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoCaptureViewModel.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoCaptureViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.levprav.videosmap.navigation.NavigationManager
 import ru.levprav.videosmap.navigation.PreviewNavigation
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +13,10 @@ class VideoCaptureScreenViewModel @Inject constructor(
     private val navigationManager: NavigationManager
 ) : ViewModel() {
     fun navigate(uri: String) {
-        navigationManager.navigate(PreviewNavigation.previewScreen(uri))
+        val uriEncoded = URLEncoder.encode(
+            uri,
+            StandardCharsets.UTF_8.toString()
+        )
+        navigationManager.navigate(PreviewNavigation.previewScreen(uriEncoded))
     }
 }
