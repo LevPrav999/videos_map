f2cd13a71faae0b5daa01c9f5d738b11261f3a4f
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-21 18:08:10 +0300

    remove Box from effect

--- app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoPreviewScreen.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoPreviewScreen.kt
@@ -2,15 +2,24 @@ package ru.levprav.videosmap.presentation.camera
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ui.StyledPlayerView
+import ru.levprav.videosmap.R
 
 @Composable
 fun VideoPreviewScreen(
@@ -25,22 +34,22 @@ fun VideoPreviewScreen(
         }
     }
 
-    DisposableEffect(
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            AndroidView(
-                factory = { context ->
-                    StyledPlayerView(context).apply {
-                        player = exoPlayer
-                    }
-                },
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-    ) {
+    DisposableEffect(Unit) {
         onDispose {
             exoPlayer.release()
         }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AndroidView(
+            factory = { context ->
+                StyledPlayerView(context).apply {
+                    player = exoPlayer
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
