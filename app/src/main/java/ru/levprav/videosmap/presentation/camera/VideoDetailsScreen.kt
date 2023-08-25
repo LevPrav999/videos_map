a392ca5caeac174586edfb625c2fadb125e924a9
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-25 21:29:42 +0300

    add circular indicator

--- app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoDetailsScreen.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/camera/VideoDetailsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -75,13 +77,15 @@ fun VideoDetailsScreen(
 
         Button(
             onClick = {
-                viewModel.saveVideo(uri, createVideoThumbnail(context, uri)!!, description)
+                if(!viewModel.state.isLoading){
+                    viewModel.saveVideo(uri, createVideoThumbnail(context, uri)!!, description)
+                }
             },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text(text = "Upload video")
+            if (viewModel.state.isLoading) CircularProgressIndicator(color = Color.White) else Text(text = "Upload video")
         }
     }
 }
