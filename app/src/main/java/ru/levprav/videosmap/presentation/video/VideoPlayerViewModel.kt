b2c886596cd09f77566337040efe93e8d7cbc8ff
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-08-30 22:01:14 +0300

    add viewModel to player screen

---
+++ app/src/main/java/ru/levprav/videosmap/presentation/video/VideoPlayerViewModel.kt
@@ -0,0 +1,160 @@
+package ru.levprav.videosmap.presentation.video
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.launch
+import ru.levprav.videosmap.domain.repository.UserRepository
+import ru.levprav.videosmap.domain.repository.VideoRepository
+import ru.levprav.videosmap.domain.util.Resource
+import ru.levprav.videosmap.navigation.NavigationDirections
+import ru.levprav.videosmap.navigation.NavigationManager
+import ru.levprav.videosmap.presentation.profile.ProfilePageState
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoPlayerViewModel  @Inject constructor(
+    private val videoRepository: VideoRepository,
+    private val userRepository: UserRepository,
+    private val navigationManager: NavigationManager
+) : ViewModel() {
+    var state by mutableStateOf(VideoPlayerState())
+        private set
+
+    fun loadVideoModel(videoId: String){
+
+        viewModelScope.launch {
+            videoRepository.getVideoById(videoId).collect { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        state = state.copy(isLoading = true)
+                    }
+
+                    is Resource.Error -> {
+                        state = state.copy(isLoading = false, error = result.message)
+                    }
+
+                    is Resource.Success -> {
+                        state = state.copy(
+                            data = result.data,
+                            isLiked = result.data!!.liked.contains(userRepository.getCurrentUserId())
+                        )
+                        loadAvatar()
+                        checkFollowing()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadAvatar(){
+        viewModelScope.launch {
+            userRepository.getProfileDetail(state.data!!.userId).collect { result ->
+                state = when (result) {
+                    is Resource.Loading -> {
+                        state.copy(isLoading = true)
+                    }
+
+                    is Resource.Error -> {
+                        state.copy(isLoading = false, error = result.message)
+                    }
+
+                    is Resource.Success -> {
+                        state.copy(
+                            avatar = result.data!!.imageUrl
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkFollowing(){
+        viewModelScope.launch {
+            userRepository.getFollowings(userRepository.getCurrentUserId()!!).collect { result ->
+                val currentUser = userRepository.getMyProfile()
+                state = when (result) {
+                    is Resource.Loading -> {
+                        state.copy(isLoading = true)
+                    }
+
+                    is Resource.Error -> {
+                        state.copy(isLoading = false, error = result.message)
+                    }
+
+                    is Resource.Success -> {
+                        state.copy(
+                            isSubscribed = result.data!!.contains(currentUser.last().data)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun setLike(){
+        state = state.copy(isLiked=state.data!!.liked.contains(userRepository.getCurrentUserId()))
+    }
+
+    fun like(videoId: String){
+        viewModelScope.launch {
+            videoRepository.like(videoId).collect{ result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        state = state.copy(isLoading = true)
+                    }
+                    is Resource.Error -> {
+                        state = state.copy(isLoading = false, error = result.message)
+                    }
+
+                    is Resource.Success -> {
+                        state = state.copy(isLoading = false, data = result.data)
+                        setLike()
+                    }
+                }
+            }
+
+        }
+    }
+
+    fun unlike(videoId: String){
+        viewModelScope.launch {
+            videoRepository.unlike(videoId).collect{ result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        state = state.copy(isLoading = true)
+                    }
+                    is Resource.Error -> {
+                        state = state.copy(isLoading = false, error = result.message)
+                    }
+
+                    is Resource.Success -> {
+                        state = state.copy(isLoading = false, data = result.data)
+                        setLike()
+                    }
+                }
+            }
+
+        }
+    }
+
+    fun follow(userId: String){
+        viewModelScope.launch {
+            userRepository.follow(userId).collect{
+                result -> if(result is Resource.Success){
+                checkFollowing()
+            }
+            }
+        }
+    }
+
+    fun navigateBack(){
+        navigationManager.navigate(NavigationDirections.mainScreen)
+    }
+}
