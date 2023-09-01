35afa41711a28603b6e3d4b799458e10bf48fd18
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-09-01 20:39:19 +0300

    add comments to model

--- app/src/main/java/ru/levprav/videosmap/domain/models/CommentModel.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/models/CommentModel.kt
@@ -3,9 +3,9 @@ package ru.levprav.videosmap.domain.models
 import java.util.Date
 
 data class CommentModel(
-    val id: String,
-    val text: String,
-    val createdAt: Date,
-    val videoId: String,
-    val userId: String
+    val id: String, // Id of comment
+    val text: String, // Text of comment
+    val createdAt: Date, // Date of comment created
+    val videoId: String, // Video Id that has been commented
+    val userId: String // User Id that posted comment
 )
