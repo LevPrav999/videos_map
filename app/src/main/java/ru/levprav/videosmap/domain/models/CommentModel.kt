c84298850e9fc25f7baf37a02a89baaed020cdf1
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-09-01 21:11:24 +0300

    add toMap/toCommentModel methods

--- app/src/main/java/ru/levprav/videosmap/domain/models/CommentModel.kt
+++ app/src/main/java/ru/levprav/videosmap/domain/models/CommentModel.kt
@@ -9,3 +9,23 @@ data class CommentModel(
     val videoId: String, // Video Id that has been commented
     val userId: String // User Id that posted comment
 )
+
+fun CommentModel.toMap(): Map<String, Any?> {
+    return mapOf(
+        "id" to id,
+        "text" to text,
+        "createdAt" to createdAt,
+        "videoId" to videoId,
+        "userId" to userId
+    )
+}
+
+fun Map<String, Any?>.toCommentModel(): CommentModel {
+    return CommentModel(
+        id = this["id"] as String,
+        text = this["text"] as String,
+        createdAt = this["createdAt"] as Date,
+        videoId = this["videoId"] as String,
+        userId = this["userId"] as String
+    )
+}
