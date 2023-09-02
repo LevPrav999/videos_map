e992aa6d6d47252f26c5c4a669c6f99e5237b95e
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-09-02 21:11:03 +0300

    add comment value to state

--- app/src/main/java/ru/levprav/videosmap/presentation/comments/CommentsState.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/comments/CommentsState.kt
@@ -5,6 +5,9 @@ import ru.levprav.videosmap.domain.models.CommentModel
 data class CommentsState(
     val comments: List<CommentModel>? = null,
     val avatars: MutableList<String>? = mutableListOf(),
+
+    val commentValue: String = "",
+
     val isLoading: Boolean = false,
     val error: String? = null,
 )
