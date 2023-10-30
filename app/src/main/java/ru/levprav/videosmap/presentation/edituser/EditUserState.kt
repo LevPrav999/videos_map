34e6c27076c55039056f4cc3c645aafa5c140e0f
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-10-30 22:09:13 +0300

    add imageUrl

--- app/src/main/java/ru/levprav/videosmap/presentation/edituser/EditUserState.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/edituser/EditUserState.kt
@@ -3,7 +3,7 @@ package ru.levprav.videosmap.presentation.edituser
 import android.net.Uri
 
 data class EditUserState(
-    val data: UserInfo = UserInfo(null, null, null, null),
+    val data: UserInfo = UserInfo(null, null, null, null, null),
     val isFromNetwork: Boolean = false,
     val isLoading: Boolean = false,
     val error: String? = null,
@@ -14,6 +14,7 @@ data class EditUserState(
 data class UserInfo(
     val username: String?,
     val description: String?,
-    val imageUrl: Uri?,
+    val imagePathUri: Uri?,
+    val imageUrl: String?,
     val imageUrlNetwork: String?,
 )
