e5bbd8d65c6b0371c14a1b95d3f77489e84c6ab0
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-10-30 22:10:16 +0300

    add launchedEffect to splash screen

--- app/src/main/java/ru/levprav/videosmap/presentation/splash/SplashScreen.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/splash/SplashScreen.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
 fun SplashScreen(viewModel: SplashViewModel) {
-
+    val context = LocalContext.current
+    LaunchedEffect(Unit){
+        viewModel.initAppwrite(context)
+    }
     Column(modifier = Modifier.fillMaxSize()) {
         Text("Splash")
     }
