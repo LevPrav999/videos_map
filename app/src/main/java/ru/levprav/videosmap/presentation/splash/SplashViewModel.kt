ab8905ae992184f78bd14d1c9f8e42346c0868bf
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2024-05-08 17:04:08 +0300

    update splash screen

--- app/src/main/java/ru/levprav/videosmap/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/ru/levprav/videosmap/presentation/splash/SplashViewModel.kt
@@ -1,11 +1,11 @@
 package ru.levprav.videosmap.presentation.splash
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import ru.levprav.videosmap.domain.repository.UserRepository
+import ru.levprav.videosmap.domain.util.Resource
 import ru.levprav.videosmap.navigation.NavigationDirections
 import ru.levprav.videosmap.navigation.NavigationManager
 import javax.inject.Inject
@@ -16,14 +16,20 @@ class SplashViewModel @Inject constructor(
     private val navigationManager: NavigationManager
 ) : ViewModel() {
 
-    fun initAppwrite(context: Context) {
+    init {
         viewModelScope.launch {
-            repository.init(context)
+            repository.getMyProfile().collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        navigationManager.navigate(NavigationDirections.authentication)
+                    }
 
-            if (repository.getCurrentUserId() != null) {
-                navigationManager.navigate(NavigationDirections.mainScreen)
-            } else {
-                navigationManager.navigate(NavigationDirections.authentication)
+                    is Resource.Success -> {
+                        navigationManager.navigate(NavigationDirections.mainScreen)
+                    }
+
+                    else -> {}
+                }
             }
         }
     }
